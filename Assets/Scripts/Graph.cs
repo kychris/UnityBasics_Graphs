c3ca15babb8643fcb9a1e507fcc7211f5ca69923
kychris <kychris1999@hotmail.com>
2021-02-03 18:31:28 -0800

    Create 3D shapes with Parametric coordinates

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -19,47 +19,46 @@ public class Graph : MonoBehaviour
         float step = 2f / resolution;
 
         // Transform variables
-        Vector3 position = new Vector3(0, 0, 0);
         var scale = Vector3.one * step;
         points = new Transform[resolution * resolution];
 
         for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+
+            // Reset x, Increment z when reach row end
+            if (x == resolution)
             {
-                // Reset x, Increment z when reach row end
-                if (x == resolution)
-                {
-                    x = 0;
-                    z += 1;
-                }
-
-                // Create point in scene
-                Transform point = Instantiate(pointPrefab);
-
-                // Define point coordinates
-                position.x = (x + 0.5f) * step - 1f;
-                position.z = (z + 0.5f) * step - 1f;
-
-                // Assign location and scale
-                point.localPosition = position;
-                point.localScale = scale;
-                point.SetParent(transform, false);
-                points[i] = point;
+                x = 0;
+                z += 1;
             }
+
+            // Create point in scene
+            Transform point = Instantiate(pointPrefab);
+
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
         }
+
     }
 
     void Update()
     {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
+        float step = 2f / resolution;
 
-        for (int i = 0; i < points.Length; i++)
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
